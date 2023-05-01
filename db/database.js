794554dd5aa4547c56db08d9e799a7606807b825
CsaB <b.f.csabesz@gmail.com>
2023-05-01 10:07:47 +0300

    corrected connection string

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/WebProg', {
+    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/WebProg', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
